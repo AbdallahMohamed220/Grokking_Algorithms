e7c27ba32a4a334bf9dcc692ccf6565e272294b9
AbdllahMohamed <devabdo2000@gmail.com>
2023-06-09 19:08:29 +0300

    first commit

---
+++ bin/ch1/binary_search.dart
@@ -0,0 +1,22 @@
+void main() {
+  print(binarySearch([1, 2, 5, 6, 7], 66));
+}
+
+binarySearch(List myList, var item) {
+  var low = 0;
+  var high = myList.length - 1;
+
+  while (low <= high) {
+    var mid = low + high;
+    var guess = myList[mid];
+
+    if (guess == item) {
+      return mid;
+    } else if (guess > item) {
+      high = mid - 1;
+    } else {
+      low = mid + 1;
+    }
+  }
+  return -1;
+}
